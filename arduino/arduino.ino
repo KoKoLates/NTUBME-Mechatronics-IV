084ae946ce5c75ae7939224df54b662255e2c227
KoKoLates <the21515@gmail.com>
2022-12-08 22:31:27 +0800

    update arduino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,21 +1,19 @@
 #include "DHT.h"
-#include <NewPing.h>
 #include <ServoTimer2.h>
 
 // Global Variable parts
 bool grabFlag = false;
+bool buzzFlag = false;
 const int maxDistance = 200;
 const int motorPin[4] = {4, 5, 6, 7};
 const int ultroPin[4] = {2, 3, 8, 9};
 // 0: stop, 1: forward, 2: right, 3: left, 4: backward
-const int motorSpeed[5][2] = {{0, 0}, {100, 100}, {100, 0},
-                              {0, 100}, {-100, -100}};
+const int motorSpeed[5][2] = {{0, 0}, {80, 80}, {80, 0},
+                              {0, 80}, {-80, -80}};
 
 // Object declaration parts
 ServoTimer2 gripper;
 DHT dht11(13, DHT11);
-NewPing ultroLeft(ultroPin[0], ultroPin[1], maxDistance);
-NewPing ultroRight(ultroPin[2], ultroPin[3], maxDistance);
 
 // Sensor Values
 float temperature = 0.0;
@@ -28,6 +26,10 @@ void setup() {
   gripper.write(angleToPulse(180));
   dht11.begin();
 
+  pinMode(ultroPin[0], OUTPUT);
+  pinMode(ultroPin[2], OUTPUT);
+  pinMode(ultroPin[1], INPUT);
+  pinMode(ultroPin[3], INPUT);
   // Timer initialize
   timerInit();  
 }
@@ -40,19 +42,21 @@ void loop() {
       digitalWrite(motorPin[3], LOW);
       analogWrite(motorPin[1], motorSpeed[str.toInt()][0]);
       analogWrite(motorPin[2], motorSpeed[str.toInt()][1]);
+      delayMicroseconds(10);
     } else if(str == "4") {
       digitalWrite(motorPin[0], HIGH);
       digitalWrite(motorPin[3], HIGH);
       analogWrite(motorPin[1], 255 + motorSpeed[str.toInt()][0]);
       analogWrite(motorPin[2], 255 +  motorSpeed[str.toInt()][1]);
+      delayMicroseconds(10);
     } else if(str == "5") {
       grabFlag ? gripper.write(angleToPulse(180)) : gripper.write(angleToPulse(90));
       grabFlag = !grabFlag;
     }
   }
   temperature = getTemperature();
-  leftDist = getDistance(ultroLeft);
-  rightDist = getDistance(ultroRight);
+  leftDist = getDistance(ultroPin[0], ultroPin[1]);
+  rightDist = getDistance(ultroPin[2], ultroPin[3]);
 }
 
 void timerInit(){
@@ -85,8 +89,33 @@ float getTemperature() {
   else return temp;
 }
 
-unsigned int getDistance(NewPing p) {
-  delay(50);
-  unsigned int dis = p.ping();
-  return p.convert_cm(dis);
+unsigned int getDistance(int trig, int echo) {
+  digitalWrite(trig, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+  int distances = pulseIn(echo, HIGH)/58.2;
+  if(distances <= 10) {
+    buzzerRing();
+  }
+  else if(buzzFlag == true) {
+    buzzerStop();
+  }
+
+  if(distances > 200){
+    distances = 200;
+  }
+  return distances;
+}
+
+void buzzerRing(){
+//  tone(11, 1000);
+  buzzFlag = true;
+  
+}
+
+void buzzerStop() {
+//  noTone(11);
+  buzzFlag = false;
 }
